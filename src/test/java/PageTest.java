0b454ba9601812814c5d75d2b0e2c35b890cb7bc
Vlajs <vladimirspasic1996@gmail.com>
2024-10-04 16:35:40 +0200

    second change

--- src/test/java/PageTest.java
+++ src/test/java/PageTest.java
@@ -3,5 +3,6 @@ public class PageTest {
         System.out.println("Mali ako ovo ne radi najeb'o si");
         System.out.println("Radi odlicno. :-)");
         System.out.println("Tako sam i mislio :D");
+        System.out.println("Hvala ti dobri covece.");
     }
 }
