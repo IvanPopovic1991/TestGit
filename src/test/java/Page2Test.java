a3453b1c3ef2db8b135a239900193405ddab17dd
Vlajs <vladimirspasic1996@gmail.com>
2024-10-11 14:37:23 +0200

    Stampanje nova poruka

--- src/test/java/Page2Test.java
+++ src/test/java/Page2Test.java
@@ -2,5 +2,6 @@ public class Page2Test {
     public void testMethod2(){
         System.out.println("Testq Testa");
         System.out.println("New code line");
+        System.out.println("Nova promena.");
     }
 }
